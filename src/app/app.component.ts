c16468396abe1926a2cbd93b00f87d35083308cb
NewSun2004 <tanbdn22406@st.uel.edu.vn>
2024-11-08 15:53:10 +0700

    Added display products component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DisplayProductPageComponent } from "./components/display-product-page/display-product-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DisplayProductPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/display-product-page/display-product-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-product-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-product-page.component.html',
+  styleUrl: './display-product-page.component.css'
+})
+export class DisplayProductPageComponent {
+
+}
