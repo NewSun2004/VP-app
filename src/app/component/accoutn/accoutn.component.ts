6bf0e0cb9c47ee6213ed662dfd50d223d8b76f59
NewSun2004 <tanbdn22406@st.uel.edu.vn>
2024-11-11 20:43:36 +0700

    "Tạo component account"

---
+++ src/app/component/accoutn/accoutn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accoutn',
+  standalone: true,
+  imports: [],
+  templateUrl: './accoutn.component.html',
+  styleUrl: './accoutn.component.css'
+})
+export class AccoutnComponent {
+
+}
