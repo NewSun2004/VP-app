a96b3836c5290f1196b419aa8a17547e71567412
NewSun2004 <tanbdn22406@st.uel.edu.vn>
2024-11-13 10:23:30 +0700

    Added various components for nav-bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { DisplayProductsComponent } from './components/display-products/display-products.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { CustomizeComponent } from './components/customize/customize.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
+  {path : "home", component : HomepageComponent},
+  {path : "aboutus", component : AboutUsComponent},
   {path : "eyeglasses", component : DisplayProductsComponent},
   {path : "eyeglasses/:productId", component : ProductDetailsComponent},
-  {path : "", redirectTo : "eyeglasses", pathMatch : "full"},
+  {path : "sunglasses", component : DisplayProductsComponent},
+  {path : "sunglasses/:productId", component : ProductDetailsComponent},
+  {path : "customize", component : CustomizeComponent},
+  {path : "accessory", component : DisplayProductsComponent},
+  {path : "accessory/:productId", component : ProductDetailsComponent},
+  {path : "contact", component : ContactComponent},
+  {path : "", redirectTo : "home", pathMatch : "full"},
+  {path : "**", component : PageNotFoundComponent}
 ];
---
+++ src/app/components/customize/customize.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customize',
+  standalone: true,
+  imports: [],
+  templateUrl: './customize.component.html',
+  styleUrl: './customize.component.css'
+})
+export class CustomizeComponent {
+
+}
