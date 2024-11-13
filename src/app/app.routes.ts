fbfea91a5bb1fb9c596abb97d071d754a01af982
NewSun2004 <tanbdn22406@st.uel.edu.vn>
2024-11-13 21:11:19 +0700

    various minor changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
-  {path : "home", component : HomepageComponent},
+  {path : "", component : HomepageComponent},
   {path : "aboutus", component : AboutUsComponent},
   {path : "eyeglasses", component : DisplayProductsComponent},
   {path : "eyeglasses/:productId", component : ProductDetailsComponent},
@@ -18,6 +18,5 @@ export const routes: Routes = [
   {path : "accessory", component : DisplayProductsComponent},
   {path : "accessory/:productId", component : ProductDetailsComponent},
   {path : "contact", component : ContactComponent},
-  {path : "", redirectTo : "home", pathMatch : "full"},
   {path : "**", component : PageNotFoundComponent}
 ];
