1582c1a0533699d71a90a07204a5a66fee3e7d4a
TinaSayHii <pnt2245@gmail.com>
2024-11-16 17:06:33 +0700

    Update product service with required changes

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,39 +3,38 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  myAPIUrl : string = "http://localhost:3000";
+  myAPIUrl: string = 'http://localhost:3000';
 
-  products : any = [];
+  products: any = [];
   productsSbuject = new BehaviorSubject([]);
 
-  constructor(private _httpClient : HttpClient) { }
+  constructor(private _httpClient: HttpClient) {}
 
-
-  getAllProduct(routeBaseUrl : string, queryParams? : any) : Observable<[]>
->>>>>>> ee7132edb0ab8114552f75dae013a161a2490bd9
-  {
+  getAllProduct(routeBaseUrl: string, queryParams?: any): Observable<[]> {
     // Default URL
-    let queryString : string = ""; // Build the query string from params if provided
-    if (queryParams)
-    {
-        queryString = new URLSearchParams(queryParams).toString();
+    let queryString: string = ''; // Build the query string from params if provided
+    if (queryParams) {
+      queryString = new URLSearchParams(queryParams).toString();
     }
 
-
-    return this._httpClient.get<[]>(this.myAPIUrl + `/${routeBaseUrl}` + `${queryString ? '?' + queryString : ''}`).pipe(
->>>>>>> ee7132edb0ab8114552f75dae013a161a2490bd9
-     tap((allProducts : any) => {
-      this.products = allProducts;
-      this.productsSbuject.next(allProducts);
-     })
-    );
+    return this._httpClient
+      .get<[]>(
+        this.myAPIUrl +
+          `/${routeBaseUrl}` +
+          `${queryString ? '?' + queryString : ''}`
+      )
+      .pipe(
+        tap((allProducts: any) => {
+          this.products = allProducts;
+          this.productsSbuject.next(allProducts);
+        })
+      );
   }
 
-  getProduct(routeFullUrl : string) : Observable<any>
-  {
+  getProduct(routeFullUrl: string): Observable<any> {
     return this._httpClient.get<[]>(this.myAPIUrl + routeFullUrl);
   }
 }
